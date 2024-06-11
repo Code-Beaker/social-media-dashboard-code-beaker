0dba2dbf784fc4a7cf6d04644e804a3911f3adcd
Tharun Raj <tharun.2.linux@gmail.com>
2024-06-11 18:48:28 +0530

    add toggle functionality

--- app/main.js
+++ app/main.js
@@ -1,2 +1,16 @@
 import "./typeScale.css";
 import "./scss/style.scss";
+
+const body = document.querySelector(".body");
+const toggleButton = document.getElementById("toggleTheme");
+const nextTheme = document.getElementById("themeName");
+
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("body-dark");
+
+  if (body.classList.contains("body-dark")) {
+    nextTheme.textContent = "Light";
+  } else {
+    nextTheme.textContent = "Dark";
+  }
+});
